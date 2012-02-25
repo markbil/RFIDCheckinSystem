2edc1485281fd93ccc4999a4db41938b64bd9795
Geek Boy Technultancy <github@geekboy.com.au>
2012-02-25 18:16:28 +1000

    Cleaned up formatting
    Added check to see if all fields have a value

--- edge_signup/signup.php
+++ edge_signup/signup.php
@@ -6,51 +6,36 @@
 </head>
 <body>
 
-<div style="width:400px; margin-left:auto; margin-right:auto;">
-<fieldset>
-<legend>Edge Signup</legend>
-<table>
+	<div style="width: 400px; margin-left: auto; margin-right: auto;">
+		<fieldset>
+			<legend>Edge Signup</legend>
+			<table>
+			<?php if(isset($_GET["fail"])) echo "Please Fill In All Fields</br>";?>
+				<form name="signup" method="post" action="singup_submit.php">
+					<tr>
+						<td>Swipe Card:</td>
+						<td><input type="text" name="swipe"
+							value="&lt;?php /*if (isset($_GET['swipe_id'])) echo $_GET['swipe_id']; else echo &quot;&quot;;*/?&gt;" />
+						</td>
+					</tr>
 
+					<tr>
+						<td>Name?</td>
+						<td><input type="text" name="name" /></td>
+					</tr>
 
-<form name="signup" method="post" action="singup_submit.php">
+					<tr>
+						<td>Password</td>
+						<td><input type="password" name="password" /></td>
+					</tr>
 
-<tr>
-<td>
-Swipe Card:
-</td>
-<td>
-<input type="text" name="swipe" value="<?php /*if (isset($_GET['swipe_id'])) echo $_GET['swipe_id']; else echo "";*/?>" />
-</td>
-</tr>
-
-<tr>
-<td>
-Name?
-</td>
-<td>
-<input type="text" name="name" />
-</td>
-</tr>
-
-<tr>
-<td>
-Password
-</td>
-<td>
-<input type="password" name="password" />
-</td>
-</tr>
-
-
-
-<tr>
-<td colspan="2" align="center">
-<input type="submit" value="Sign Up" />
-</td>
-</tr>
-</form>
-</table>
-</fieldset>
-</div>
+					<tr>
+						<td colspan="2" align="center"><input type="submit"
+							value="Sign Up" /></td>
+					</tr>
+				</form>
+			</table>
+		</fieldset>
+	</div>
 </body>
 </html>
