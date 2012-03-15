7e4a1053be8b31f703f2f41604a147582ecfd004
markbil <mabil@web.de>
2012-03-15 14:57:47 +1000

    ammended so it servers as an interface for the Processing "RFID_CheckinApp.pde"

--- checkin_submit_manual.php
+++ checkin_submit_manual.php
@@ -11,25 +11,45 @@ $checkintime = $dateTime->format("Y-m-d H:i:s");
 $im_type = 1; // 1 = RFID
 $im_thirdpartyid = $_GET["thirdpartyid"]; // unique RFID number
 
-$sql_get_im_id = "SELECT id FROM identification_media im WHERE im.thirdpartyid = ".$im_thirdpartyid." AND im.type = " . $im_type;
+$sql_get_im_id = "SELECT id FROM identification_media im WHERE im.thirdpartyid = '".$im_thirdpartyid."' AND im.type = " . $im_type;
 
-if($_GET["thirdpartyid"] != "") {
-	$query = 'INSERT INTO check_in (MainLocation, SubLocation, Check_In_Time, identification_media_id) VALUES (\''.$mainlocation.'\', \''.$sublocation.'\', \''.$checkintime.'\',('.$sql_get_im_id.'))';
-	
-	echo $query;
-	echo "</br>";
-	echo "</br>";
-	$result = @mysql_query ($query) or die(mysql_error());
-	//echo $result;
-	
-	echo "MySQL INSERT successful!";
-	
-} else
-{
-	echo "RFID parameter not set! </br>
-	
-	Checkin call must have following format: .../checkin_submit_manual.php?rfid=xxx&sublocation=yyy";
-	
+	if($_GET["thirdpartyid"] != "") {
+		$query = 'INSERT INTO check_in (MainLocation, SubLocation, Check_In_Time, identification_media_id) VALUES (\''.$mainlocation.'\', \''.$sublocation.'\', \''.$checkintime.'\',('.$sql_get_im_id.'))';
+		
+		echo $query;
+		echo "</br>";
+		echo "</br>";
+		//*** mysql error text between the three stars will be displayed to the user in the processing sketch *** 
+		$result = @mysql_query ($query) or die("***FAIL***  +++RFID card is not assigend to a user yet. Please visit a staff member at the counter...+++  " . mysql_error());
+		//echo $result;
+		$insertid = mysql_insert_id();
+		
+		echo "***SUCCESS***" . "     mysql insert-id: " . $insertid;
+		echo "</br>";
+		echo "</br>";
+		
+			//get the firstname, lastname, time, etc of the user who just checked in...
+			$query2 = "SELECT eu.id AS edge_user_id, eu.firstname, eu.lastname, imt.name AS imt_name, im.ThirdPartyID AS im_id, check_in.check_in_time AS checkin_timestamp, check_in.sublocation AS checkin_sublocation FROM ((((identification_media im JOIN identification_media_type imt ON im.type = imt.id) JOIN people ON people.identification_id = im.id) JOIN edge_users eu ON eu.id = people.edge_users_id) JOIN check_in ON check_in.identification_media_id = im.id) WHERE check_in.checkinid=" . $insertid;
+		
+			echo $query2;
+			echo "</br>";
+			echo "</br>";
+			//*** mysql error text between the three stars will be displayed to the user in the processing sketch ***
+			$result2 = @mysql_query ($query2) or die("***Checkin could not be found***  " . mysql_error());
+			
+			while($row = mysql_fetch_array($result2))
+			{
+				//data to be read by the processing script wrappen in +++ and +++
+				echo "+++" . $row['firstname'] . "+++";
+				echo "<br />";
+			}
+				
+			//echo "***Check-in successful!***";
+
+		
+	} else{
+		echo "RFID parameter not set! </br>
+		Checkin call must have following format: .../checkin_submit_manual.php?thirdpartyid=xxx&sublocation=yyy";
 	}
 
 
