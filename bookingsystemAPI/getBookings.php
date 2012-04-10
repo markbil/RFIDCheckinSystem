d1fb0ed04f05fd293586a6727bd3b2634e3cf900
markbil <mabil@web.de>
2012-04-10 17:57:59 +1000

    Parser for the Edge's Window Bay and Event Booking system. basic skeleton, needs to be amended...

---
+++ bookingsystemAPI/getBookings.php
@@ -0,0 +1,39 @@
+<?php
+
+$client = new SoapClient("http://sl-queensland-web.ungerboeck.com/USI_BK_Service/USI_Service.asmx?WSDL");
+
+
+$bookingParameters  = array(
+		'OrgCode'   => "10",
+		'EventID'   => "",
+		'FromDate'   => "2011-01-01",
+		'ToDate'   => "2012-12-01",
+);
+
+$authentication  = array(
+		'PartnerKey'   => "A456woiuer89asdfAPX8",
+		'PartnerHash'   => "",
+);
+
+$params   = array(
+		'ServiceAuthentication'   => $authentication,
+		'Parameters'   => $bookingParameters,
+);
+
+
+
+$result = $client->GetBookingInfo($params);
+
+print_r($result);
+
+
+// $soapclient = new SoapClient('http://sl-queensland-web.ungerboeck.com/USI_BK_Service/USI_Service.asmx?WSDL');
+// echo '<pre>';
+// 	var_dump($soapclient->__getFunctions());	
+// echo '</pre>';
+// echo '<pre>';
+// 	var_dump($soapclient->__getTypes());
+// echo '</pre>';
+
+?>
+
