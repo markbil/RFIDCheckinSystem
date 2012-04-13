07c750e32267df77cc248b9ca649b72ea76b36cd
markbil <mabil@web.de>
2012-04-13 14:24:23 +1000

    - all bugs fixed - SeeedRFIDLib replaced by manually reading UART - v1. reads RFID-card via UART and submits a checkin using the CheckinAPI - works on SNARC using SoftSerial RX 7

--- SNARC/SNARC_CheckinClient/SNARC_CheckinClient.ino
+++ SNARC/SNARC_CheckinClient/SNARC_CheckinClient.ino
@@ -6,61 +6,53 @@
  
  Code based on:
  
- - Arduino Ethernet Client sketch by bildr.org: http://bildr.org/2011/06/arduino-ethernet-client/
- - "SeedRFIDLIb" library by Johann Richard https://github.com/johannrichard/SeeedRFIDLib
- 
+ - RFID reader code by
+     - Martijn The - http://www.martijnthe.nl/ 
+     - BARRAGAN - http://people.interaction-ivrea.it/h.barragan 
+     - HC Gilje - http://hcgilje.wordpress.com/resources/rfid_id12_tagreader/
+     - Martijn The - http://www.martijnthe.nl/
+- Arduino Ethernet Client sketch by bildr.org: http://bildr.org/2011/06/arduino-ethernet-client/
+
+RFID-Reader and wiring according to:
+     - http://www.seeedstudio.com/wiki/Electronic_brick_-_125Khz_RFID_Card_Reader
+     - set jumper on the RFID reader to UART mode
+     - connect TX of RFID reader to a RX of the Arduino. define SoftSerial accordingly
  */
 
 
 //ARDUINO 1.0+ ONLY
-//ARDUINO 1.0+ ONLY
 #include <Ethernet.h>
 #include <SPI.h>
-#include <SeeedRFIDLib.h>
 #include <SoftwareSerial.h>
 
 
 ////////////////////////////////////////////////////////////////////////
 //CONFIGURE RFID READER
 ////////////////////////////////////////////////////////////////////////
-    // Connect the Reader's TX to the RX Pin on the Arduino. Use 
-    #define RFID_RX_PIN 99
-    #define RFID_TX_PIN 7 //set 0 if Softserial RX should match D0 on the Arduino (handy e.g. for SNARC as wiring is then possible with one connector)
     
     // Configure the Library in UART Mode
-    //SeeedRFIDLib RFID(RFID_RX_PIN, RFID_TX_PIN);
-    
-    // Configure the Library in WIEGAND Mode
-    SeeedRFIDLib RFID(WIEGAND_26BIT); 
-    RFIDTag tag;
+    SoftwareSerial mySerial(7, 8); // 7-Rx, 8=Tx
+
 
 ////////////////////////////////////////////////////////////////////////
 //CONFIGURE ETHERNET
 ////////////////////////////////////////////////////////////////////////
     byte server[] = { 192, 168, 0, 14 }; //ip Address of the server you will connect to
-    
-    //The location to go to on the server
-    //make sure to keep HTTP/1.0 at the end, this is telling it what type of file it is    
-    
-    //String location = "/TheEdge_VisitorProfiles/API/view_list_distinctusercheckins_all_2.php HTTP/1.0";
-    
-    // if need to change the MAC address (Very Rare)
     byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+    
+    EthernetClient client;
+    char inString[32]; // string for incoming serial data
+    int stringPos = 0; // string index counter
+    boolean startRead = false; // is reading?
 
 
 ////////////////////////////////////////////////////////////////////////
 //CONFIGURE API SETTINGS
 ////////////////////////////////////////////////////////////////////////
     int im_type = 1; // should be set to 1 on every RFID reader. 1 stands for RFID in the Checkin-DB
-    String thirdpartyid = "9999999"; //actual RFID number. set random number for test purposes...
     int sublocation = 3; //set location key according to where the RFID reader is installed, e.g. 1 for Window Bays 1.
+//    String thirdpartyid = "9999999"; //actual RFID number. set random number for test purposes...
 
-
-EthernetClient client;
-
-char inString[32]; // string for incoming serial data
-int stringPos = 0; // string index counter
-boolean startRead = false; // is reading?
 
 //FEEDBACK LEDs
 int yellowpin = A2; //led to register keypad pressses
@@ -77,61 +69,111 @@ void setup(){
   pinMode(redpin, OUTPUT); //define led pin as output
   pinMode(speakerPin, OUTPUT);
   
-  Ethernet.begin(mac);
   Serial.begin(9600);
-  Serial.println("Serial Ready");
+  mySerial.begin(9600);
   
-
+  Serial.println("Serial...");  
+  Serial.println("Ethernet...");
+  Ethernet.begin(mac);
+  Serial.println("Setup finished.");
+  
 }
 
 void loop(){
   
-  
-  
 /////UART MODE
-//  if(RFID.isIdAvailable()) {
-//    tag = RFID.readId();
-//    Serial.print("ID:       ");
-//    Serial.println(tag.id);
-//    Serial.print("ID (HEX): ");
-//    Serial.println(tag.raw);
-//    delay(1000);
-//
-//    String pageValue = connectAndRead(); //connect to the server and read the output
-//    Serial.println(pageValue); //print out the findings.
-//
-//  }
-
-//////WIEGAND MODE
+  byte i = 0;
+  byte val = 0;
+  byte code[6];
+  byte checksum = 0;
+  byte bytesread = 0;
+  byte tempbyte = 0;
+  String rfid = "";
   if(!writingToDB){
-    if(RFID.isIdAvailable()) {
-        writingToDB = true;
-            tag = RFID.readId();
-            // In Wiegand Mode, we only get the card code
-            Serial.print("CC = ");
-            String rfid_tagid = String(tag.id); 
-            Serial.println(rfid_tagid); 
+    if(mySerial.available() > 0) {
+      writingToDB = true;
+      if((val = mySerial.read()) == 2) {                  // check for header 
+        bytesread = 0; 
+        while (bytesread < 12) {                        // read 10 digit code + 2 digit checksum
+          if( mySerial.available() > 0) { 
+            val = mySerial.read();
+            if((val == 0x0D)||(val == 0x0A)||(val == 0x03)||(val == 0x02)) { // if header or stop bytes before the 10 digit reading 
+              break;                                    // stop reading
+            }
             
-            String url_base = "/TheEdge_VisitorProfiles/checkin_submit_manual.php?";
-            String url_param1 = "im_type=" + String(im_type);
-            String url_param2 = "&thirdpartyid=" + rfid_tagid;
-            String url_param3 = "&sublocation=" + String(sublocation);
-            String url_httptail = " HTTP/1.0";
-            
-            String url_complete = url_base + url_param1 + url_param2 + url_param3 + url_httptail;
-            Serial.println("url: " + url_complete);
-            
-            blinkPin(yellowpin, 500); //acknowledge that RFID card has been read
-              
-          String pageValue = connectAndRead(url_complete); //connect to the server and read the output
-          Serial.println(pageValue); //print out the findings.
+            // Do Ascii/Hex conversion:
+            if ((val >= '0') && (val <= '9')) {
+              val = val - '0';
+            } else if ((val >= 'A') && (val <= 'F')) {
+              val = 10 + val - 'A';
+            }
+  
+            // Every two hex-digits, add byte to code:
+            if (bytesread & 1 == 1) {
+              // make some space for this hex-digit by
+              // shifting the previous hex-digit with 4 bits to the left:
+              code[bytesread >> 1] = (val | (tempbyte << 4));
+  
+              if (bytesread >> 1 != 5) {                // If we're at the checksum byte,
+                checksum ^= code[bytesread >> 1];       // Calculate the checksum... (XOR)
+              };
+            } else {
+              tempbyte = val;                           // Store the first hex digit first...
+            };
+  
+            bytesread++;                                // ready to read next digit
+          } 
+        } 
+  
+        // Output to Serial:
+  
+        if (bytesread == 12) {                          // if 12 digit read is complete
+          //Serial.print("5-byte code: ");
+          for (i=0; i<6; i++) {
+            if (code[i] < 16){
+              //Serial.print("0");
+              rfid = rfid + "0";
+            }
+            //Serial.print(code[i], HEX);
+            rfid = rfid + String(code[i], HEX);
+          }  
           
-          delay(2000);
-        writingToDB = false;
+          //Serial.println();
+          Serial.print("Checksum: ");
+          Serial.print(code[5], HEX);
+          if (code[5] == checksum){
+            Serial.println(" -- passed.");
+            
+              rfid.toUpperCase();
+              Serial.println("RFID: " + rfid);
+              
+              String url_base = "/TheEdge_VisitorProfiles/checkin_submit_manual.php?";
+              String url_param1 = "im_type=" + String(im_type);
+              String url_param2 = "&thirdpartyid=" + rfid;
+              String url_param3 = "&sublocation=" + String(sublocation);
+              String url_httptail = " HTTP/1.0";
+              
+              String url_complete = url_base + url_param1 + url_param2 + url_param3 + url_httptail;
+              Serial.println("url: " + url_complete);
+              
+              blinkPin(yellowpin, 500); //acknowledge that RFID card has been read
+                
+            String pageValue = connectAndRead(url_complete); //connect to the server and read the output
+            Serial.println(pageValue); //print out the findings.
+              
+          }
+          else
+            Serial.println(" -- error.");
+          
+          Serial.println();
+        }
+  
+        bytesread = 0;
+      }
+      //delay(2000);
+      writingToDB = false;
     }
   }
-  
-
 }
 
 String connectAndRead(String url){
@@ -147,7 +189,6 @@ String connectAndRead(String url){
     client.println();
 
     blinkPin(greenpin, 500); //flash green LED to confirm successful connection
-//    triggerSpeakerPattern(2);
     return readPage(); //go and read the output
 
   }else{
