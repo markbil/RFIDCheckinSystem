b8b1a16ab993a7a88de88668b16843f14715b8ab
markbil <mabil@web.de>
2012-05-11 16:38:33 +1000

    api description updated with links to new functions

--- API/index.php
+++ API/index.php
@@ -48,18 +48,18 @@ with specific datasets.
 <p>
 <div class="function_name">view_list_distinctusercheckins_all_2.php</div>
 <div class="function_description">Description: same as "view_list_distinctusercheckins_all.php" but includes user's level of expertise for each expertise keyword</div>
-<a href="<?php echo $urlbase ?>view_list_distinctusercheckins_all.php"><?php echo $urlbase ?>view_list_distinctusercheckins_all.php</a>
+<a href="<?php echo $urlbase ?>view_list_distinctusercheckins_all_2.php"><?php echo $urlbase ?>view_list_distinctusercheckins_all_2.php</a>
 </p>
 
 <p>
     <div class="function_name">view_number_distinctusercheckins_perhour.php</div>
-    <div class="function_description">Description: returns hours of a day and the number of distinct users that have checked-in at at that hour</div>
+    <div class="function_description">Description: returns hours of a day and the number of distinct users that have checked-in at at that hour cumulated over all dates since launch of the checkin system</div>
     <a href="<?php echo $urlbase ?>view_number_distinctusercheckins_perhour.php"><?php echo $urlbase ?>view_number_distinctusercheckins_perhour.php</a>
 </p>
 
 <p>
 <div class="function_name">view_number_distinctusercheckins_perhour_15minslots.php</div>
-<div class="function_description">Description: returns hours of a day in 15min sections, and number of checkins that distinct users have checked-in at each 15min slot during a particuar hour. E.g. at 8am, 6 people have checked in during the 1st quarter of the hour (i.e. 8:00-8:14am)</div>
+<div class="function_description">Description: returns hours of a day in 15min sections, and number of checkins that distinct users have checked-in at each 15min slot during a particuar hour, cumulated over all dates since launch of the checkin system. E.g. at 8am, 6 people have checked in during the 1st quarter of the hour (i.e. 8:00-8:14am)</div>
 <a href="<?php echo $urlbase ?>view_number_distinctusercheckins_perhour_15minslots.php"><?php echo $urlbase ?>view_number_distinctusercheckins_perhour_15minslots.php</a>
 </p>
 
@@ -67,6 +67,12 @@ with specific datasets.
 <div class="function_name">view_number_distinctusercheckins_perdate.php</div>
 <div class="function_description">Description: returns checkin_dates and the number of distinct users that have checked-in on that date</div>
 <a href="<?php echo $urlbase ?>view_number_distinctusercheckins_perdate.php"><?php echo $urlbase ?>view_number_distinctusercheckins_perdate.php</a>
+</p>
+
+<p>
+<div class="function_name">view_number_distinctusercheckins_perdate_hour_quarter.php</div>
+<div class="function_description">Description: returns checkin_dates and the number of distinct users that have checked-in per hour and quarter of an hour on each date</div>
+<a href="<?php echo $urlbase ?>view_number_distinctusercheckins_perdate_hour_quarter.php"><?php echo $urlbase ?>view_number_distinctusercheckins_perdate_hour_quarter.php</a>
 </p>
 
 <p>
