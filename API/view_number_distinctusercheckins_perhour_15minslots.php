ada7b9366fb869e6f4f9d0c9e1ab73482de80fea
markbil <mabil@web.de>
2012-05-11 12:30:55 +1000

    initial commit

---
+++ API/view_number_distinctusercheckins_perhour_15minslots.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once ('mysql_connection.php');
+require_once ('timezone.php');
+ 
+$arr = array();
+//VIEW: "view_number_distinctusercheckins_perhour" = returns hours of a day and number of checkins that distinct users have checkin at at that our
+$query = "SELECT hour, distinct_usercheckins FROM view_number_distinctusercheckins_perhour_15minslots";
+
+$tmz = mysql_query ("SET time_zone = " . $timezone) or die("mysql error: " . mysql_error());
+$rs = mysql_query ($query) or die("mysql error: " . mysql_error()); 
+
+
+while($obj = mysql_fetch_assoc($rs)) {
+		
+	$arr[] = $obj;
+
+}
+ 
+
+echo json_encode($arr);
+
+?>
