64de0262d4ed9d480576b9c09c83875ba9af11a4
Geek Boy Technultancy <github@geekboy.com.au>
2012-07-04 19:53:48 +1000

    Merged with Daniels changes

--- userprofile/codeigniter_application/views/edge_user/profile.php
+++ userprofile/codeigniter_application/views/edge_user/profile.php
@@ -1,4 +1,4 @@
-<?php echo validation_errors('<div class="login-warning"><span class="center">', '</span></div>'); ?>
+<?php echo validation_errors('<div class="login-warning center"><span class="center">', '</span></div>'); ?>
 <?php 
 	if ($message) {
 		echo '<div class="logg-success"><span class="center">';
